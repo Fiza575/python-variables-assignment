e4cff3a90e11293563582021a220be02cecc0247
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-05-05 18:32:50 +0500

    Add files via upload

---
+++ variables_practice.py
@@ -0,0 +1,15 @@
+# Part 1: Declare variables
+name = "Fiza Arzoo Khalid"
+age = 26
+student_id = "0017"
+
+# Part 2: Invalid variables
+1st_name = "Fiza"  # Cannot start with a number
+second name = "Arzoo" # Spaces are not allowed
+last$name = "Khalid" # Special characters are not allowed
+
+# Part 3: Practice
+country_name = "Pakistan"
+book_title = "The Alchemist"
+is_adult = True
+podcast_channel = " The Diary of CEO"
